0c61c5b32ec2123ad7bcb75160d9b97be4b21d1e
bilal1960 <bilal45001@hotmail.com>
2024-03-22 20:18:55 +0100

    mise en forme de la connexion

--- app_auth/views.py
+++ app_auth/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib import messages
-
+from .forms import LoginForm
 def login_blog(request):
-    if request.method =="POST":
-        username = request.POST['username']
-        pwd = request.POST['pwd']
-        user=authenticate(username=username,password=pwd)
-        if user is not None:
-            return redirect("home")
-        else:
-            messages.error(request,"Erreur d'authentification")
-            return render(request, "login.html")
-        
-    return render(request,"login.html")
+   if request.method == "POST":
+      form = LoginForm(request.POST)
+      if form.is_valid():
+         username = form.cleaned_data['username']
+         pwd = form.cleaned_data["pwd"]
+         user = authenticate(username=username,password=pwd)
+         if user is not None:
+            return redirect('home')
+         else:
+            messages.error(request,'Authentification échoué')
+            return render(request,'login.html',{'form':form})
+      else:
+          for field in form.errors:
+              form[field].field.widget.attrs['class'] += ' is-invalid'
+          return render(request,'login.html',{'form':form})     
+   else:
+      form = LoginForm()
+      return render(request,"login.html",{"form":form})
+         
+      
